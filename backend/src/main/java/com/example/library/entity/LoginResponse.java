3a7c8d80831f8322abb1d158d3e86174077a329b
wanganfudge <hehangyu7004@163.com>
2026-06-21 14:27:50 +0800

    Upload backend/src/main/java/com/example/library/entity/LoginResponse.java

---
+++ backend/src/main/java/com/example/library/entity/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.example.library.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private boolean success;
+    private String message;
+    private String token;  // 简单令牌
+}
